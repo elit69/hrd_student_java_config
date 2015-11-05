1df8f8201375631c0364fd77d99070a9bee9e2eb
Chhorn Elit <chhornelit@gmail.com>
2015-11-05 08:39:04 +0700

    dgsdf

--- src/main/java/com/lit/hw/services/impl/StudentServiceImpl.java
+++ src/main/java/com/lit/hw/services/impl/StudentServiceImpl.java
@@ -19,9 +19,9 @@ public class StudentServiceImpl implements StudentService {
 
 	public ArrayList<Student> list() {
 		String sql = "select * from student ORDER BY id;";
-		try (Connection cnn = dataSource.getConnection();
-				PreparedStatement ps = cnn.prepareStatement(sql);
-				ResultSet rs = ps.executeQuery();) {
+		try (Connection cnn = dataSource.getConnection();) {
+			PreparedStatement ps = cnn.prepareStatement(sql);
+			ResultSet rs = ps.executeQuery();
 			System.out.println(ps.toString());
 			ArrayList<Student> listStudent = new ArrayList<Student>();
 			while (rs.next()) {
@@ -42,8 +42,8 @@ public class StudentServiceImpl implements StudentService {
 	public boolean add(Student stu) {
 		String sql = "INSERT INTO student(first_name,last_name,classroom) "
 				+ "VALUES (?,?,?);";
-		try (Connection cnn = dataSource.getConnection();
-				PreparedStatement ps = cnn.prepareStatement(sql);) {
+		try (Connection cnn = dataSource.getConnection();) {
+			PreparedStatement ps = cnn.prepareStatement(sql);
 			ps.setString(1, stu.getFirstname());
 			ps.setString(2, stu.getLastname());
 			ps.setString(3, stu.getClassroom());
@@ -59,8 +59,8 @@ public class StudentServiceImpl implements StudentService {
 	public boolean update(Student stu) {
 		String sql = "update student set " + "first_name = ?, "
 				+ "last_name = ?, " + "classroom= ? " + "where id = ?";
-		try (Connection cnn = dataSource.getConnection();
-				PreparedStatement ps = cnn.prepareStatement(sql);) {
+		try (Connection cnn = dataSource.getConnection();) {
+			PreparedStatement ps = cnn.prepareStatement(sql);
 			ps.setString(1, stu.getFirstname());
 			ps.setString(2, stu.getLastname());
 			ps.setString(3, stu.getClassroom());
@@ -76,8 +76,8 @@ public class StudentServiceImpl implements StudentService {
 
 	public boolean delete(int stuId) {
 		String sql = "Delete from student where id = ?";
-		try (Connection cnn = dataSource.getConnection();
-				PreparedStatement ps = cnn.prepareStatement(sql);) {
+		try (Connection cnn = dataSource.getConnection();) {
+			PreparedStatement ps = cnn.prepareStatement(sql);
 			ps.setInt(1, stuId);
 			System.out.println(ps.toString());
 			if (ps.executeUpdate() > 0)
@@ -90,10 +90,10 @@ public class StudentServiceImpl implements StudentService {
 
 	public Student show(int stuId) {
 		String sql = "select * from student where id = ?";
-		try (Connection cnn = dataSource.getConnection();
-				PreparedStatement ps = cnn.prepareStatement(sql);
-				ResultSet rs = ps.executeQuery();) {
+		try (Connection cnn = dataSource.getConnection();) {
+			PreparedStatement ps = cnn.prepareStatement(sql);
 			ps.setInt(1, stuId);
+			ResultSet rs = ps.executeQuery();
 			System.out.println(ps.toString());
 			if (rs.next()) {
 				Student s = new Student();
@@ -112,11 +112,11 @@ public class StudentServiceImpl implements StudentService {
 	public ArrayList<Student> search(String keyword, String type) {
 		String sql = "select * from student where LOWER(" + type
 				+ ") like LOWER(?) ORDER BY " + type;
-		try (Connection cnn = dataSource.getConnection();
+		try (Connection cnn = dataSource.getConnection();) {
 			PreparedStatement ps = cnn.prepareStatement(sql);
-			ResultSet rs = ps.executeQuery();) {
 			System.out.println(ps.toString());
 			ps.setString(1, keyword + "%");
+			ResultSet rs = ps.executeQuery();
 			ArrayList<Student> listStudent = new ArrayList<Student>();
 			while (rs.next()) {
 				Student s = new Student();
